ca3669cc140651b670f48c27f5333bff97e56ff2
Elena <skyoriver@mail.ru>
2023-10-21 14:46:12 +0300

    fetch added

---
+++ script.js
@@ -0,0 +1,24 @@
+let url='https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1';
+
+
+
+function fetchTableData(url) {
+  fetch(url) // здесь нужно указать путь к файлу с данными
+    .then(response => response.json())
+    .then(data => {
+      const table = document.getElementById('table');
+ 
+      data.forEach(item => {
+        const row = table.insertRow();
+        const idCell = row.insertCell();
+        const symbolCell=row.insertCell();
+        const nameCell = row.insertCell();
+        idCell.textContent = item.id;
+        symbolCell.textContent = item.symbol;
+        nameCell.textContent=item.name;
+      });
+    })
+    .catch(error => console.error('Ошибка:', error));
+}
+
+fetchTableData(url)
